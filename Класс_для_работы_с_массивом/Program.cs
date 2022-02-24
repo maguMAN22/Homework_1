3c7f014feea3ab08511b85853f7d693b536ec048
Gleb <glebanov7991@gmail.com>
2022-02-24 18:19:33 +0300

    HW4

---
+++ Класс_для_работы_с_массивом/Program.cs
@@ -0,0 +1,79 @@
+﻿//Глеб Филиппов
+
+//а) Дописать класс для работы с одномерным массивом. Реализовать конструктор, создающий массив определенного размера и заполняющий массив
+//числами от начального значения с заданным шагом. Создать свойство Sum, которое возвращает сумму элементов массива, метод Inverse, возвращающий новый массив с
+//измененными знаками у всех элементов массива (старый массив, остается без изменений), метод Multi,
+//умножающий каждый элемент массива на определённое число, свойство MaxCount,
+//возвращающее количество максимальных элементов.
+
+
+
+public class MyArrays
+{
+    int[] stArray;
+    int Count;
+    int Fnum;
+    int Step;
+
+
+    public MyArrays(int count, int fnum, int step)
+    {
+        Count = count;
+        Fnum = fnum;
+        Step = step;
+        stArray = new int[Count];
+        for (int i = 0; i < count; i++)
+        {
+            stArray[i] = fnum+(step * i);
+        }
+    }
+
+    public int Sum(int[] arr)
+    {
+        int b = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            b+=arr[i];
+        }
+        return b;
+    }
+
+    public int[] Inverse(int[] arr)
+    {
+        int[] newArr = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            newArr[i] = -arr[i];
+        }
+
+
+        return newArr;
+    }
+
+    public int[] Multi(int[] arr, int mlt)
+    {
+        int[] newArr = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            newArr[i] = mlt * arr[i];
+        }
+
+        return newArr;
+    }
+
+    public int MaxCount(int[] arr)
+    {
+        int max = arr.Max();
+        int b = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == max)
+                b += 1;
+        }
+
+        return b;
+    }
+
+}
+
+
